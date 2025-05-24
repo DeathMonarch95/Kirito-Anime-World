ac02b23db6aeb2907f06e8f69cf05ee395068cfc
DeathMonarch95 <yc.walanju2006@gmail.com>
2025-05-24 16:42:58 +0530

    Feat: Finalize basic PWA implementation

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'; // This import is correct
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+// This line should be outside the render method, after the render call
+serviceWorkerRegistration.register();      
